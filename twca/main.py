5ebeec74c2c3adcc006337143183708be9fa5ca6
Artifter <as.stanek@gmail.com>
2021-02-27 19:09:19 +0100

    now accepting scan types

--- twca/main.py
+++ twca/main.py
@@ -5,6 +5,44 @@ import requests
 nm = nmap.PortScanner()
 
 
+class Scanner:
+    def __init__(self, sS=False, sV=False, sT=False, sU=False, sX=False, target='127.0.0.1', ports='--p 22-443'):
+        self.sS = sS
+        self.sV = sV
+        self.sT = sT
+        self.sU = sU
+        self.sX = sX
+        self.target = target
+        self.ports = ports
+    
+    def get_scan_types(self):
+        scan_type = str()
+        if self.sS != False:
+            scan_type += '-sS '
+        if self.sV != False:
+            scan_type += '-sV '
+        if self.sT != False:
+            scan_type += '-sT '
+        if self.sU != False:
+            scan_type += '-sU '
+        if self.sX != False:
+            scan_type += '-sX '
+        return scan_type
+
 if __name__ == '__main__':
-    output = nm.scan('127.0.0.1', '22-443')
-    pp.pprint(output)
+    lubina = Scanner(sS=True, sV=True)
+    print(lubina.get_scan_types())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
