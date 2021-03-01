ad257c967bd6b57a046cb1d968dc0f1a1482d311
Artifer <as.stanek@gmail.com>
2021-03-01 10:44:00 +0100

    further improvements with main scan methods

--- twca/main.py
+++ twca/main.py
@@ -6,7 +6,7 @@ nm = nmap.PortScanner()
 
 
 class Scanner:
-    def __init__(self, sS=False, sV=False, sT=False, sU=False, sX=False, target='127.0.0.1', ports='--p 22-443'):
+    def __init__(self, sS=False, sV=False, sT=False, sU=False, sX=False, target='127.0.0.1', ports='22-443'):
         self.sS = sS
         self.sV = sV
         self.sT = sT
@@ -15,10 +15,29 @@ class Scanner:
         self.target = target
         self.ports = ports
     
+    def get_scan_type(self):
+        args = str()
+        if self.sS == True:
+            args += '-sS '
+        if self.sV == True:
+            args += '-sV '
+        if self.sT == True:
+            args += '-sT '
+        if self.sU == True:
+            args += '-sU '
+        if self.sX == True:
+            args += '-sX '
+        return args
+    
+    def scan(self):
+        out = nm.scan(hosts=self.target, arguments=self.get_scan_type()+f'-p {self.ports}')
+        return out
+
+
 
 if __name__ == '__main__':
     lubina = Scanner(sS=True, sV=True)
-    print(lubina.start_scan())
+    print(lubina.scan())
 
 
 
